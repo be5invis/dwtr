8c1212ca2ffb295bbaa3e1a224237e1e6724c705
Belleve <belleve@typeof.net>
2022-06-12 22:28:11 -0700

    Optimization

--- src/svg_text_render.rs
+++ src/svg_text_render.rs
@@ -3,7 +3,7 @@ use core::fmt::Write;
 use minidom::Element;
 use std::{
     cell::RefCell,
-    collections::{hash_map::Entry, HashMap},
+    collections::{btree_map::Entry, BTreeMap},
 };
 use windows::{
     core::{AsImpl, IUnknown, Interface, Result},
@@ -62,7 +62,7 @@ impl SvgRun {
 
 struct SvgDataStorage {
     last_path_id: usize,
-    path_defs: HashMap<String, usize>,
+    path_defs: BTreeMap<String, usize>,
     runs: Vec<SvgRun>,
 }
 
@@ -70,7 +70,7 @@ impl SvgDataStorage {
     fn new() -> Self {
         Self {
             last_path_id: 0,
-            path_defs: HashMap::new(),
+            path_defs: BTreeMap::new(),
             runs: Vec::new(),
         }
     }
@@ -373,18 +373,33 @@ impl IDWriteTextRenderer1_Impl for SvgTextRenderer {
 pub(crate) struct SvgGeometrySink {
     scalar: f32,
     body: RefCell<String>,
+    last_x: RefCell<f32>,
+    last_y: RefCell<f32>,
 }
+
+const COORD_RESOLUTION: f32 = 0x100 as f32;
 
 impl SvgGeometrySink {
     fn new(scalar: f32) -> Self {
         Self {
             scalar,
             body: RefCell::new(String::new()),
+            last_x: RefCell::new(0.0),
+            last_y: RefCell::new(0.0),
         }
     }
 
     fn reset(&self) -> String {
         self.body.replace(String::new())
+    }
+
+    fn process_coord(&self, f: f32) -> f32 {
+        (f * self.scalar * COORD_RESOLUTION).round() / COORD_RESOLUTION
+    }
+
+    fn set_last_point(&self, x: f32, y: f32) {
+        self.last_x.replace(x);
+        self.last_y.replace(y);
     }
 }
 
@@ -393,26 +408,28 @@ impl ID2D1SimplifiedGeometrySink_Impl for SvgGeometrySink {
     fn SetFillMode(&self, _fill_mode: D2D1_FILL_MODE) {}
     fn SetSegmentFlags(&self, _flags: D2D1_PATH_SEGMENT) {}
     fn BeginFigure(&self, start_point: &D2D_POINT_2F, _figure_begin: D2D1_FIGURE_BEGIN) {
-        write!(
-            self.body.borrow_mut(),
-            "M {} {} ",
-            start_point.x * self.scalar,
-            start_point.y * self.scalar
-        )
-        .unwrap()
+        let cx_orig = start_point.x;
+        let cy_orig = start_point.y;
+        {
+            let cx = self.process_coord(cx_orig);
+            let cy = self.process_coord(cy_orig);
+            write!(self.body.borrow_mut(), "M {} {} ", cx, cy).unwrap();
+        }
+        self.set_last_point(cx_orig, cy_orig);
     }
     fn AddLines(&self, points: *const D2D_POINT_2F, points_count: u32) {
         let mut sink = self.body.borrow_mut();
         for i in 0..points_count {
             unsafe {
                 let point = points.offset(i as isize);
-                write!(
-                    sink,
-                    "L {} {} ",
-                    (*point).x * self.scalar,
-                    (*point).y * self.scalar
-                )
-                .unwrap()
+                let cx_orig = (*point).x;
+                let cy_orig = (*point).y;
+                {
+                    let cx = self.process_coord(cx_orig);
+                    let cy = self.process_coord(cy_orig);
+                    write!(sink, "L {} {} ", cx, cy).unwrap();
+                }
+                self.set_last_point(cx_orig, cy_orig);
             }
         }
     }
@@ -421,17 +438,39 @@ impl ID2D1SimplifiedGeometrySink_Impl for SvgGeometrySink {
         for i in 0..beziers_count {
             unsafe {
                 let curve = beziers.offset(i as isize);
-                write!(
-                    sink,
-                    "C {} {} {} {} {} {} ",
-                    (*curve).point1.x * self.scalar,
-                    (*curve).point1.y * self.scalar,
-                    (*curve).point2.x * self.scalar,
-                    (*curve).point2.y * self.scalar,
-                    (*curve).point3.x * self.scalar,
-                    (*curve).point3.y * self.scalar
-                )
-                .unwrap()
+
+                let x0_orig = self.last_x.borrow().clone();
+                let y0_orig = self.last_y.borrow().clone();
+                let x1_orig = (*curve).point1.x;
+                let y1_orig = (*curve).point1.y;
+                let x2_orig = (*curve).point2.x;
+                let y2_orig = (*curve).point2.y;
+                let x3_orig = (*curve).point3.x;
+                let y3_orig = (*curve).point3.y;
+
+                let xm1 = x0_orig + (x1_orig - x0_orig) * 1.5;
+                let ym1 = y0_orig + (y1_orig - y0_orig) * 1.5;
+                let xm2 = x3_orig + (x2_orig - x3_orig) * 1.5;
+                let ym2 = y3_orig + (y2_orig - y3_orig) * 1.5;
+                {
+                    let x1 = self.process_coord(x1_orig);
+                    let y1 = self.process_coord(y1_orig);
+                    let x2 = self.process_coord(x2_orig);
+                    let y2 = self.process_coord(y2_orig);
+                    let x3 = self.process_coord(x3_orig);
+                    let y3 = self.process_coord(y3_orig);
+
+                    if COORD_RESOLUTION * (xm2 - xm1).abs() < 1.0
+                        && COORD_RESOLUTION * (ym2 - ym1).abs() < 1.0
+                    {
+                        let xm = self.process_coord((xm1 + xm2) / 2.0);
+                        let ym = self.process_coord((ym1 + ym2) / 2.0);
+                        write!(sink, "Q {} {} {} {} ", xm, ym, x3, y3).unwrap();
+                    } else {
+                        write!(sink, "C {} {} {} {} {} {} ", x1, y1, x2, y2, x3, y3).unwrap();
+                    }
+                }
+                self.set_last_point(x3_orig, y3_orig);
             }
         }
     }
